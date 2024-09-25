1cce8e98173f49165483ba56da4e066c46de81fe
candyap <candyap182@icloud.com>
2024-09-25 23:00:45 +0700

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Candy Pangestu on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListAppTests/TodoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppTests.swift
+//  TodoListAppTests
+//
+//  Created by Candy Pangestu on 9/25/24.
+//
+
+import Testing
+@testable import TodoListApp
+
+struct TodoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
